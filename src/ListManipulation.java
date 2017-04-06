222d80dfb92cb9efd0e9563cdf7ce4d5665be72d
Lengyel Norbert <lengyelnorbert@hotmail.com>
2017-04-06 17:27:36 +0200

    remove feature completed

--- src/ListManipulation.java
+++ src/ListManipulation.java
@@ -9,35 +9,45 @@ import java.util.List;
 public class ListManipulation {
 
   public void addItemToList(String[] addItem) {
-    if (addItem.length == 1) {
-      System.out.println("Unable to add: no task provided");
-    } else {
-      String elementToAdd;
-      Path todoDataFilePath = Paths.get("./tododatas.txt");
-      elementToAdd = addItem[1];
-      try {
+    try {
+      if (addItem.length == 1) {
+        System.out.println("Unable to add: no task provided");
+      } else {
+        String elementToAdd;
+        Path todoDataFilePath = Paths.get("./tododatas.txt");
+        elementToAdd = addItem[1];
+
         List<String> allItems = Files.readAllLines(todoDataFilePath);
         allItems.add(elementToAdd);
         Files.write(todoDataFilePath, allItems);
-      } catch (IOException e) {
-        System.out.println("Something went wrong with opening the data txt.");
       }
+    } catch (IOException e) {
+      System.out.println("Something went wrong with opening the data txt.");
     }
   }
+
 
   public void removeItemFromList(String[] removeItem) {
     Path todoDataFilePath = Paths.get("./tododatas.txt");
     List<String> allItems = new ArrayList<>();
     try {
       allItems = Files.readAllLines(todoDataFilePath);
-    } catch (IOException e) {
+
+      if (removeItem.length == 1) {
+        System.out.println("Unable to remove: no index provided");
+      } else if (allItems.size() < Integer.parseInt(removeItem[1])) {
+        System.out.println("Unable to remove: index is out of bound");
+      } else {
+        int itemToRemove = Integer.parseInt(removeItem[1])-1;
+        allItems.remove(itemToRemove);
+        Files.write(todoDataFilePath, allItems);
+      }
+
+    } catch (IOException ex) {
       System.out.println("Something went wrong with opening the data txt.");
-    }
-    if (removeItem.length == 1) {
-      System.out.println("Unable to remove: no index provided");
-    }
-    if (removeItem.length != 1 && allItems.size() < Integer.parseInt(removeItem[1])) {
-      System.out.println("Unable to remove: index is out of bound");
+    } catch (Exception e) {
+      System.out.println("Unable to remove: index is not a number");
     }
   }
 }
+
